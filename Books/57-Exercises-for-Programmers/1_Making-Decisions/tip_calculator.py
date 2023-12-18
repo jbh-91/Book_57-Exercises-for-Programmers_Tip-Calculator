a0dfc8dd97bbf9f840df079fd031161de90fa754
Julian Heinen <mail@julian-heinen.de>
2023-12-18 22:47:50 +0100

    Update custom round function round_to_2_decimals

--- Books/57-Exercises-for-Programmers/1_Making-Decisions/tip_calculator.py
+++ Books/57-Exercises-for-Programmers/1_Making-Decisions/tip_calculator.py
@@ -1,10 +1,17 @@
 from icecream import ic
+from math import floor, ceil
 
-def round_up(float_num: float) -> float:
-    # TODO: Custom round function for educational purposes
-    # if float_num - 0.5 == float_num:
-    
-    return round(float_num, 2)
+# Custom round function for educational purposes, usually just use "round(float_num, 2)
+def round_to_2_decimals(float_num: float) -> float:
+    # WONTFIX: Floating Point Math is wonky https://0.30000000000000004.com
+    #          bill_amount = 11.25
+    #          tip_rate = 10
+    #          total_amount = 12.370000000000001
+    if floor(float_num - 0.5) == int(float_num // 1):
+        return ceil(float_num * 100) / 100
+    else:
+        return floor(float_num * 100) / 100
+
 
 def calculate_tip_amount(amount: float | int, rate: int) -> float:
     return amount * rate / 100
@@ -14,11 +21,11 @@ def calculate_total_amount(bill_amount: float, tip: float) -> float:
 
 
 if __name__ == "__main__":
-    bill_amount = float(input())
-    tip_rate = int(input())
-    
-    tip_amount = calculate_tip_amount(bill_amount, tip_rate)
+    bill_amount = float(input("Please enter your bill amount:\n> "))
+    tip_rate = int(input("Please enter your tip rate (as percentage):\n> "))
 
+    tip_amount = round_to_2_decimals(calculate_tip_amount(bill_amount, tip_rate))
     total_amount = calculate_total_amount(bill_amount, tip_amount)
-    ic(f"{tip_amount:.2f} EUR")
-    ic(f"{total_amount:.2f} EUR")
+
+    ic(f"{tip_amount} EUR")
+    ic(f"{total_amount} EUR")
