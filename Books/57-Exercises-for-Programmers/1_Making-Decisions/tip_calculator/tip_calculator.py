4290404b94a33899582f0a243ae6dfcec10f27b3
Julian Heinen <mail@julian-heinen.de>
2023-12-19 20:23:25 +0100

    Change folder structure for main module

--- Books/57-Exercises-for-Programmers/1_Making-Decisions/tip_calculator/tip_calculator.py
+++ Books/57-Exercises-for-Programmers/1_Making-Decisions/tip_calculator/tip_calculator.py
@@ -1,7 +1,6 @@
-from icecream import ic
+# Custom round function for educational purposes, usually just use "round(float_num, 2)
 from math import floor, ceil
 
-# Custom round function for educational purposes, usually just use "round(float_num, 2)
 def round_to_2_decimals(float_num: float) -> float:
     # WONTFIX: Floating Point Math is wonky https://0.30000000000000004.com
     #          bill_amount = 11.25
@@ -19,13 +18,15 @@ def calculate_tip_amount(amount: float | int, rate: int) -> float:
 def calculate_total_amount(bill_amount: float, tip: float) -> float:
     return tip + bill_amount
 
+def render_bill(bill_amount: float, tip_rate: float) -> None:
+    tip_amount = round_to_2_decimals(calculate_tip_amount(bill_amount, tip_rate))
+    total_amount = calculate_total_amount(bill_amount, tip_amount)
+
+    print(f"{tip_amount} EUR")
+    print(f"{total_amount} EUR")
 
 if __name__ == "__main__":
     bill_amount = float(input("Please enter your bill amount:\n> "))
     tip_rate = int(input("Please enter your tip rate (as percentage):\n> "))
 
-    tip_amount = round_to_2_decimals(calculate_tip_amount(bill_amount, tip_rate))
-    total_amount = calculate_total_amount(bill_amount, tip_amount)
-
-    ic(f"{tip_amount} EUR")
-    ic(f"{total_amount} EUR")
+    render_bill(bill_amount, tip_rate)
